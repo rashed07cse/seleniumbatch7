3041e1e2dd8d22f7f27446d665cf0619bbae3184
rashedul.islam <rashedmbstucse07@gmail.com>
2023-11-17 22:24:29 +0600

    Handling Drop down list value

---
+++ src/test/java/HandlingDropDownList.java
@@ -0,0 +1,29 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public class HandlingDropDownList {
+    public static WebDriver driver;
+
+    public static void main(String[] args) {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        driver.get("https://demoqa.com/select-menu");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,150)", "");
+
+        WebElement element = driver.findElement(By.id("oldSelectMenu"));
+        Select select = new Select(element);
+        select.selectByIndex(2);
+
+    }
+
+}
